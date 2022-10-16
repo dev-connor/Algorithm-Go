671e6e2f985c4ac1963257856b480a644b7bc24f
Connor <kor.connor@gmail.com>
2022-10-17 00:51:35 +0900

    힙 추가

---
+++ main/main.go
@@ -0,0 +1,21 @@
+package main
+
+//func main() {
+//	array := [...]int{1, 2, 3, 4, 5}
+//	slice := []int{1, 2, 3, 4, 5}
+//	list := list.New()
+//	ring := ring2.New(5)
+//	map2 := make(map[int]string)
+//	map2 = map[int]string{
+//		1: "first",
+//		2: "second",
+//		3: "third",
+//	}
+//
+//	fmt.Printf("%T\n", slice)
+//	fmt.Printf("%T\n", array)
+//	fmt.Printf("%T\n", list)
+//	fmt.Printf("%T\n", ring)
+//	fmt.Printf("%T\n", map2)
+//
+//}
