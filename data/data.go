48b4ddfbdf5f80cf59a039de4f67f92b5acab3d7
Connor <kor.connor@gmail.com>
2022-10-17 22:17:28 +0900

    strings.Repeat

---
+++ data/data.go
@@ -0,0 +1,24 @@
+package data
+
+func data() {
+	//	array := [...]int{1, 2, 3, 4, 5}
+	//	slice := []int{1, 2, 3, 4, 5}
+	//	list := list.New()
+	//	ring := ring2.New(5)
+	//	map2 := make(map[int]string)
+	//map2 := map[int]string{
+	//	1: "first",
+	//	2: "second",
+	//	3: "third",
+	//}
+	//
+	//	fmt.Printf("%T\n", slice)
+	//	fmt.Printf("%T\n", array)
+	//	fmt.Printf("%T\n", list)
+	//	fmt.Printf("%T\n", ring)
+	//	fmt.Printf("%T\n", map2)
+
+	//map2[7] = "four"
+	//fmt.Println(map2)
+
+}
